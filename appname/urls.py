830d5f0bb8d582552a333a599247a8177f01e976
Абель-Амир <kubanichbekuuluabelamir@gmail.com>
2021-12-09 21:25:38 +0600

    дз 2 джанго

--- appname/urls.py
+++ appname/urls.py
@@ -4,5 +4,6 @@ from . views import *
 urlpatterns = [
     path("url_1/",aizirek_1),
     path("url_2/",aizirek_2),
-    path("url_3/",aizirek_3)
+    path("url_3/",aizirek_3),
+    path("comment/",comment_show)
 ]
